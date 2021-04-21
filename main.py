2345662306218b7ba4d43ec42a05c8fe3d74843a
MDev <matthew03.carreon@gmaik.com>
2021-04-21 10:08:13 -0700

    Start main functionality to check the letter in the list of letters

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 #Procedures
 def randomWord(words):
   chosenWord = words[random.randint(0, len(words)-1)]
-  print("DEBUG: the word chosen is at " + chosenWord)
+  print("DEBUG: the word chosen is " + chosenWord)
   return chosenWord
 
 def wipeScreen():
@@ -18,9 +18,9 @@ def indexWord(wordChosen):
     index.append(letter)
   return index
 
-def letterCheck(word):
-  for i in range(0, len(word)-1):
-    print(Test)
+def letterCheck(letter, wordIndex):
+  for i in range(0, len(wordIndex)-1):
+    print("Test")
     print("    |   ")
     print("    |   ")
     print("   ()   ")
@@ -32,7 +32,10 @@ wordList = ["house", "design", "export", "market", "resort", "winner"]
 wordLetterIndex = [""]
 numOfGuesses = 0
 wrongGuesses = 0
+chedkAn = 0
 name = ""
+gameSession = True
+newAnswer = ""
 #main
 
 name = input("What is your name?\n")
@@ -41,7 +44,14 @@ print("Hangman is where you guess the letter by typing it in")
 print("Becareful though as it slowly creates a stickfigure when guessing incorrectly!")
 wordChose = randomWord(wordList)
 print(wordChose)
-wordLetterIndex = indexWord(wordChose)
-print("Debug: Index of the chosen word is ")
-'''for x in wordLetterIndex:
+wordLetterIndex = indexWord(wordChose)cghch
+'''print("Debug: Index of the chosen word is ")
+for x in wordLetterIndex:
   print(x)'''
+while (gameSession == True):
+  userInput = input("What letter what you like to type in? (Only ONE!)")
+  for i in range(0, len(wordLetterIndex)-1):
+    if (userInput == i):
+      checkAn = checkAn+1
+  print("DEBUG: Completed Loop")
+  print(checkAn)
